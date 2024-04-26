066a47ed6b3f6e7816228da250efb2cb33437fdc
fatma <faatmaaahmedd12@gmail.com>
2024-04-26 18:56:45 +0300

    register page auth part 2

--- lib/screens/register_page.dart
+++ lib/screens/register_page.dart
@@ -72,9 +72,37 @@ class RegisterPage extends StatelessWidget {
             CustomButton(
               ontap: () async {
                 //var auth = FirebaseAuth.instance; you can you the instant directly instead of create object
-                UserCredential user = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                    email: email!, password: password!);
-                print(user.user!.displayName);
+                try {
+                  UserCredential user = await FirebaseAuth.instance
+                      .createUserWithEmailAndPassword(
+                          email: email!, password: password!);
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'weak-password') {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('The password provided is too weak.'),
+                      ),
+                    );
+                  } else if (e.code == 'email-already-in-use') {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content:
+                            Text('The account already exists for that email.'),
+                      ),
+                    );
+                  }
+                } catch (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(e.toString()),
+                    ),
+                  );
+                }
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('your registertion is successed'),
+                  ),
+                );
               },
               text: 'register ',
             ),
