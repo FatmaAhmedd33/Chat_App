a367f04ae2de4bd899934f9a701164b3398a7f53
fatma <faatmaaahmedd12@gmail.com>
2024-05-08 20:39:49 +0300

    future builder

--- lib/screens/chat_page.dart
+++ lib/screens/chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/constants.dart';
+import 'package:chat_app/helper/show_snack_bar.dart';
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -14,54 +15,65 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kPrimaryColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              kLogo,
-              width: 50,
-              height: 50,
-            ),
-            const Text(
-              'chat',
-              style: TextStyle(color: Colors.white),
-            )
-          ],
-        ),
-        centerTitle: true,
-        automaticallyImplyLeading: false, //to disappear the arrow
-      ),
-      body: Column(
-          //will throw exception bec listview in side the column do to solve this problem wrap histview whit expanded widget
-          children: [
-            Expanded(
-              child: ListView.builder(itemBuilder: (context, index) {
-                return ChatBubble();
-              }),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                controller: controller, //to clear text field after send message
-                onSubmitted: (value) {
-                  messages.add({'message': value});
-                  controller.clear();
-                },
-                decoration: InputDecoration(
-                    hintText: 'send message',
-                    suffixIcon: Icon(
-                      Icons.send,
-                      color: kPrimaryColor,
+    return FutureBuilder<QuerySnapshot>(
+        future: messages.get(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            print(snapshot.data!.docs[1]
+                ['message']); // to make sure i access the data
+            return Scaffold(
+              appBar: AppBar(
+                backgroundColor: kPrimaryColor,
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      kLogo,
+                      width: 50,
+                      height: 50,
                     ),
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(color: kPrimaryColor),
-                        borderRadius: BorderRadius.circular(12))),
+                    const Text(
+                      'chat',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+                centerTitle: true,
+                automaticallyImplyLeading: false, //to disappear the arrow
               ),
-            )
-          ]),
-    );
+              body: Column(
+                  //will throw exception bec listview in side the column do to solve this problem wrap histview whit expanded widget
+                  children: [
+                    Expanded(
+                      child: ListView.builder(itemBuilder: (context, index) {
+                        return ChatBubble();
+                      }),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextField(
+                        controller:
+                            controller, //to clear text field after send message
+                        onSubmitted: (value) {
+                          messages.add({'message': value});
+                          controller.clear();
+                        },
+                        decoration: InputDecoration(
+                            hintText: 'send message',
+                            suffixIcon: Icon(
+                              Icons.send,
+                              color: kPrimaryColor,
+                            ),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(color: kPrimaryColor),
+                                borderRadius: BorderRadius.circular(12))),
+                      ),
+                    )
+                  ]),
+            );
+          } else {
+            return Text('loading ...');
+          }
+        });
   }
 }
